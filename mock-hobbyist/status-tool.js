3b6d2d4784a520d87e95218e457893b82b8b34fc
Jacob Hensley <jacobrhensley3@gmail.com>
2020-07-15 15:08:40 -0500

    complete status tool

--- mock-hobbyist/status-tool.js
+++ mock-hobbyist/status-tool.js
@@ -1,47 +1,46 @@
 console.log("status-tool.js")
 
-$('#interested').click(function() {
-    interested()
+$(document).ready(function() {
+    close($('#interested'));
+    open($('#learning'));
+    close($('#hobbyist'));
 });
 
-$('#learning').click(function() {
-    learning()
+$('#interested').click(function () {
+    open($('#interested'))
+    close($('#learning'))
+    close($('#hobbyist'));
 });
 
-$('#hobbyist').click(function() {
-    hobbyist()
+$('#learning').click(function () {
+    close($('#interested'))
+    open($('#learning'))
+    close($('#hobbyist'))
 });
 
+$('#hobbyist').click(function () {
+    close($('#interested'))
+    close($('#learning'))
+    open($('#hobbyist'))
+});
 
-function interested() {
-    $('#interested').addClass('s8');
-    $('#interested').removeClass('s2');
-
-    $('#learning').addClass('s2');
-    $('#learning').removeClass('s8');
-
-    $('#hobbyist').addClass('s2');
-    $('#hobbyist').removeClass('s8');
+function open(status) {
+    status.addClass('s10');
+    status.removeClass('s1');
+    status.addClass('lighten-5');
+    status.removeClass('darken-4');
+    status.addClass('ease');
+    status.children('.content').show();
+    status.children('.title').show();
+    status.children('.title').removeClass('rotate');
 }
 
-function learning() {
-    $('#interested').addClass('s2');
-    $('#interested').removeClass('s8');
-
-    $('#learning').addClass('s8');
-    $('#learning').removeClass('s2');
-
-    $('#hobbyist').addClass('s2');
-    $('#hobbyist').removeClass('s8');
-}
-
-function hobbyist() {
-    $('#interested').addClass('s2');
-    $('#interested').removeClass('s8');
-
-    $('#learning').addClass('s2');
-    $('#learning').removeClass('s8');
-
-    $('#hobbyist').addClass('s8');
-    $('#hobbyist').removeClass('s2');
+function close(status) {
+    status.addClass('s1');
+    status.removeClass('s10');
+    status.addClass('darken-4');
+    status.removeClass('lighten-5');
+    status.addClass('ease');
+    status.children('.content').hide();
+    status.children('.title').addClass('rotate');
 }
